55f53352f35bb317b2cc81d0d5161598044c4a84
Yiğit Gülmez <ygulmez873@gmail.com>
2025-07-08 17:31:19 +0300

    Update card.ts

--- types/card.ts
+++ types/card.ts
@@ -1,6 +1,7 @@
 export type CardType = 'number' | 'block' | 'reverse' | 'draw_two' | 'wild' | 'wild_draw_four';
 export type CardColor = 'red' | 'green' | 'blue' | 'yellow' | null;
 export type CardNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | null;
+export type IconProps = { color: string; size: number };
 
 export interface Card {
   _id: string;
