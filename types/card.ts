336d2e69558ca57d300c9e1328b60b2d10ad5e61
Yiğit Gülmez <ygulmez873@gmail.com>
2025-07-08 17:27:26 +0300

    Update card.ts

--- types/card.ts
+++ types/card.ts
@@ -1,5 +1,6 @@
-export type CardType = 'number' | 'skip' | 'reverse' | 'draw_two' | 'wild' | 'wild_draw_four';
+export type CardType = 'number' | 'block' | 'reverse' | 'draw_two' | 'wild' | 'wild_draw_four';
 export type CardColor = 'red' | 'green' | 'blue' | 'yellow' | null;
+export type CardNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | null;
 
 export interface Card {
   _id: string;
