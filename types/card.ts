bb27aab36b81dccf98c4e9bcba06736442dfe445
Yiğit Gülmez <ygulmez873@gmail.com>
2025-07-08 17:56:31 +0300

    Update card.ts

--- types/card.ts
+++ types/card.ts
@@ -1,11 +1,55 @@
-export type CardType = 'number' | 'block' | 'reverse' | 'draw_two' | 'wild' | 'wild_draw_four';
-export type CardColor = 'red' | 'green' | 'blue' | 'yellow' | null;
+export type CardType =
+  | "number"
+  | "block"
+  | "reverse"
+  | "draw_two"
+  | "wild"
+  | "wild_draw_four";
+
+export type CardColor = "red" | "green" | "blue" | "yellow" | null;
+
 export type CardNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | null;
-export type IconProps = { color: string; size: number };
+
+export type IconProps = {
+  color: string;
+  size: number;
+};
 
 export interface Card {
   _id: string;
   type: CardType;
   color: CardColor;
-  value: number | null;    // 0-9 or null
+  value: CardNumber;
 }
+
+type NumberCardProps = {
+  type: "number";
+  number: Exclude<CardNumber, null>;
+  icon?: never;
+  color: Exclude<CardColor, null>;
+};
+
+type ColoredIconCardProps = {
+  type: "block" | "reverse" | "draw_two";
+  icon: CardType;
+  number?: never;
+  color: Exclude<CardColor, null>;
+};
+
+type WildCardProps = {
+  type: "wild" | "wild_draw_four";
+  icon: CardType;
+  number?: never;
+  color?: null;
+};
+
+type BaseProps = {
+  width?: number;
+  height?: number;
+};
+
+export type CardSvgProps = BaseProps & (
+  | NumberCardProps
+  | ColoredIconCardProps
+  | WildCardProps
+);
